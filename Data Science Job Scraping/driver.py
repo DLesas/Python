b9d301074a233f970adf57640ebfb50bcd324024
DLesas <dlesas@hotmail.com>
2022-09-05 08:49:50 +0100

    Add files via upload

---
+++ Data Science Job Scraping/driver.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium_stealth import stealth
+
+
+def make_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument('--disable-blink-features=AutomationControlled')
+
+    # options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=options, executable_path="chromedriver.exe")
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        "source": """
+        Object.defineProperty(navigator, 'webdriver', {
+          get: () => undefined
+        })
+      """
+    })
+    stealth(driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
+
+    return driver
